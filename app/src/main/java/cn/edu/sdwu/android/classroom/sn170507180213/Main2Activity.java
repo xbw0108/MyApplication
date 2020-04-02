cf811b5988b980df9c254161fe77fcd3933e92c9
xbw0108 <804037849@qq.com>
2020-04-02 21:44:33 +0800

    四月二号增加了邮件，电话，xy轴和跳转页面

--- app/src/main/java/cn/edu/sdwu/android/classroom/sn170507180213/Main2Activity.java
+++ app/src/main/java/cn/edu/sdwu/android/classroom/sn170507180213/Main2Activity.java
@@ -1,14 +1,22 @@
 package cn.edu.sdwu.android.classroom.sn170507180213;
 
 import android.app.WallpaperManager;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class Main2Activity extends AppCompatActivity {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main2Activity extends AppCompatActivity implements View.OnFocusChangeListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +32,7 @@ public class Main2Activity extends AppCompatActivity {
         //3)调用事件源的  方法注册事件监听器
         button.setOnClickListener(new MyClickListener());
 
-        //使用内部匿名类注册监听器
+        //3)使用内部匿名类注册监听器
         imageView.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View view) {
@@ -36,12 +44,52 @@ public class Main2Activity extends AppCompatActivity {
                     Log.e(Main2Activity.class.toString(),e.toString());
                 }
 
-                return false;
+                return true;
+            }
+        });
+
+        EditText email=(EditText) findViewById(R.id.ch4_1_email);
+        email.setOnFocusChangeListener(this);
+
+        LinearLayout linearLayout=(LinearLayout) findViewById(R.id.ch4_1_ll);
+        linearLayout.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                float x = motionEvent.getX();
+                float y = motionEvent.getY();
+                TextView textView=(TextView) findViewById(R.id.ch4_1_tv);
+                textView.setText("x:"+x+"y:"+y);
+                return true;
             }
         });
 
 
+
+
     }
+
+    @Override
+    public void onFocusChange(View view, boolean b) {
+        //参数b代表是否获取焦点
+        //判断邮箱地址的合法性
+        EditText editText=(EditText)view;
+        if(!b){
+            String content=editText.getText().toString();//判断edittext的内容
+            //判断油箱合法性的正则表达式
+            String regEx1 = "^([a-z0-9A-Z]+[-|\\.]?)+[a-z0-9A-Z]@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-zA-Z]{2,}$";
+            Pattern pattern=Pattern.compile(regEx1);
+            Matcher matcher = pattern.matcher(content);
+            TextView textView=(TextView) findViewById(R.id.ch4_1_tv);
+            if (matcher.matches()){
+                textView.setText("");
+            }else{
+                textView.setText("eamil invalidate");
+
+            }
+        }
+
+    }
+
     //2)实现事件监听类
     class MyClickListener implements View.OnClickListener{
         @Override
@@ -50,6 +98,13 @@ public class Main2Activity extends AppCompatActivity {
 
         }
 
-
     }
+
+    public void startMain(View view){
+        //页面跳转
+        Intent intent=new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+
+
 }
