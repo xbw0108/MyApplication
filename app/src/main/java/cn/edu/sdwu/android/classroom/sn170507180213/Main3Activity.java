c00fae3a314647f52e17a0479faa524e07a2754f
xbw0108 <804037849@qq.com>
2020-04-07 23:51:46 +0800

    .

---
+++ app/src/main/java/cn/edu/sdwu/android/classroom/sn170507180213/Main3Activity.java
@@ -0,0 +1,12 @@
+package cn.edu.sdwu.android.classroom.sn170507180213;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
